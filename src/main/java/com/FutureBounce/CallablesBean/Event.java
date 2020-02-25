dbdf7e4cb4991ff12addc9d98eebe3e941797ae7
joyyue <live_orevil@126.com>
2020-02-25 23:43:02 +0800

    FutureTask Callback Event Class Test2

---
+++ src/main/java/com/FutureBounce/CallablesBean/Event.java
@@ -0,0 +1,23 @@
+package com.FutureBounce.CallablesBean;
+
+
+/**
+ * Create by Joyyue sheting on 2020/2/23
+ */
+public class Event {
+
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String Comby(String a, String b) {
+        return a + b;
+    }
+
+}
